2368fc3dfac9e1fd00716a4ce0a6ef773f6d743b
Ethan <tyskybro@icloud.com>
2020-02-11 15:29:50 -0800

    sending project
    mlem

---
+++ Online_Attempt1/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+       transform.position = Vector3.up * speed * Input.GetAxisRaw("Vertical");
+       print("is it working");
+    }
+}
